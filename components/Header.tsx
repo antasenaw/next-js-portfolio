e0b2847e41dfa03a21b1411af1581a683f467cc3
antasena <wisenugeraha@gmail.com>
2025-11-12 23:58:09 +0700

    Set some sizings to suit mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ const Header = ({ homeRef, aboutRef, projectsRef, contactsRef }: RefProps) => {
   return (
     <>
     <header>
-      <div className='bg-[rgba(255,255,255,0.125)] border border-[rgba(255,255,255,0.5)] p-6 rounded-2xl shadow-[0px_6px_3px_rgba(0,0,0,0.2)] frutiger'>
+      <div className='bg-[rgba(255,255,255,0.125)] border border-[rgba(255,255,255,0.5)] py-4 px-6 md:p-6 rounded-2xl shadow-[0px_6px_3px_rgba(0,0,0,0.2)] frutiger'>
         <h1 className='text-[1.25rem] md:text-[2rem] font-bold'>Muhammad Iqbal Nugraha</h1>
         <p>Junior Fullstack Developer</p>
       </div>
